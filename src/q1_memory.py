a57d6b08708a66679719b2fc0ad4eca35727ef86
Jose David Gutierrez Salas <jose.gutierrez.matrix@gmail.com>
2024-03-21 19:56:30 -0500

    Desarrollo de q1_memory, q1_time y challenge

--- src/q1_memory.py
+++ src/q1_memory.py
@@ -1,5 +1,37 @@
 from typing import List, Tuple
 from datetime import datetime
+import pandas as pd
 
 def q1_memory(file_path: str) -> List[Tuple[datetime.date, str]]:
-    pass
+    #Leendo el json
+    archivo = pd.read_json(file_path, lines=True)
+    archivo = archivo[['date', 'user']]
+    
+    #Convertir el campo datetime a date
+    archivo['date'] = archivo['date'].dt.date
+    
+    # Convertir la columna de JSON en un DataFrame usando json_normalize
+    df_user = pd.json_normalize(archivo['user'])
+    
+    # Unir el DataFrame resultante con el DataFrame original
+    archivo = pd.concat([archivo,df_user],axis=1)
+    
+    # Eliminar la columna 'user' para liberar memoria
+    del archivo['user']
+
+    #Se identifica las top10 fechas 
+    resultado = archivo.groupby('date').size().reset_index(name='conteo').sort_values(by='conteo', ascending=False)
+    resultado = resultado.head(10)
+    
+    #Se unifica el dataframe con las top10 date con el archivo original
+    archivo = pd.merge(archivo, resultado, how='inner', left_on='date', right_on='date')
+    
+    # Agrupar por fecha y usuario y contar los conteos
+    archivo = archivo.groupby(['date', 'username']).size().reset_index(name='conteo')
+
+    # Para cada fecha, encontrar el username con el mayor conteo
+    archivo = archivo.loc[archivo.groupby('date')['conteo'].idxmax()]
+
+    valores_tupla = [tuple(x) for x in archivo[['date', 'username']].values]
+    return valores_tupla
+    
--- src/q1_time.py
+++ src/q1_time.py
@@ -1,5 +1,39 @@
 from typing import List, Tuple
 from datetime import datetime
+import pandas as pd
+
 
 def q1_time(file_path: str) -> List[Tuple[datetime.date, str]]:
-    pass
+    # Leer el archivo JSON
+    archivo = pd.read_json(file_path, lines=True)
+    archivo = archivo[['date', 'user']]
+    
+    # Convertir el campo datetime a date
+    archivo['date'] = archivo['date'].dt.date
+    
+    # Convertir la columna de JSON en un DataFrame usando json_normalize
+    df_user = pd.json_normalize(archivo['user'])
+    
+    # Unir el DataFrame resultante con el DataFrame original
+    archivo = pd.concat([archivo, df_user], axis=1)
+    
+    # Eliminar la columna 'user' para liberar memoria
+    archivo.drop(columns=['user'], inplace=True)
+    
+    # Identificar las top 10 fechas 
+    top_fechas = archivo['date'].value_counts().nlargest(10).index
+    
+    # Filtrar el DataFrame original para las top 10 fechas
+    df_filtrado = archivo[archivo['date'].isin(top_fechas)]
+    
+    # Agrupar por fecha y usuario y contar los conteos
+    conteos = df_filtrado.groupby(['date', 'username']).size().reset_index(name='conteo')
+    
+    # Para cada fecha, encontrar el username con el mayor conteo
+    usuarios_mas_frecuentes = conteos.loc[conteos.groupby('date')['conteo'].idxmax()]
+    
+    # Convertir a lista de tuplas
+    valores_tupla = list(usuarios_mas_frecuentes[['date', 'username']].itertuples(index=False, name=None))
+    
+    return valores_tupla
+    
